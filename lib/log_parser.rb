4a2a38454d4632ece1d8a48a0b3dad656fb7a15f
Guilherme <guiheurich@gmail.com>
2019-07-16 14:28:59 +0100

    Refactors parse method

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -2,11 +2,9 @@ class LogParser
 
   def parse(file)
     parsed_file = File.readlines file
-    parsed_lines = []
     parsed_file.map do |line|
-      parsed_lines.push(line.split(" ")[0])
+      line.split(" ")[0]
     end
-    parsed_lines
   end
-  
+
 end
